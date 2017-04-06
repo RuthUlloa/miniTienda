711cbdd19805023c668ec9297f978e650ca2b5de
Siul mongalo <siulemanuel@hotmail.com>
2017-04-06 10:27:21 -0600

    Proyecto inicial Mongo MVC

---
+++ testProyecto1BD2/WebApplication1/Models/MongoDB.cs
@@ -0,0 +1,23 @@
+﻿
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1
+{
+    public class MongoDB
+    {
+        private MongoClient cliente = new MongoClient();
+
+        public List<Producto> getProductos()
+        {
+            var db = cliente.GetDatabase("miniTiendaDB");
+
+            return db.GetCollection<Producto>("Inventario").AsQueryable().ToList();
+        }
+
+       
+    }
+}
---
+++ testProyecto1BD2/WebApplication1/Models/producto.cs
@@ -0,0 +1,22 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1
+{
+    public class Producto
+    {
+        public ObjectId Id { get; set; }
+
+        public int IDArticulo { get; set; }
+
+        public string nombreArticulo { get; set; }
+
+        public string descripcion { get; set; }
+
+    }
+}
